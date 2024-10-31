1decc7db48a7fdfb54172200757d3693b9e4c475
RogelioGB <rogeliogranadino@gmail.com>
2024-10-31 11:10:55 -0400

    Hack python-1 done

--- hack_10.py
+++ hack_10.py
@@ -4,5 +4,16 @@ text: "fooziman" output => ["F","0","0","Z","1","M","@","N"]
 
 def fn_hack_10():
     result = "fooziman"
-    #...
+    result_dic = {
+        'f': 'F',
+        'o': '0',
+        'o': '0',
+        'z': 'Z',
+        'i': '1',
+        'm': 'M',
+        'a': '@',
+        'n': 'N'
+    }
+    result = [result_dic.get(char, char) for char in result]
     return result  
+
--- hack_4.py
+++ hack_4.py
@@ -4,5 +4,6 @@ text: "fooziman" output => "foozimaN"
 
 def fn_hack_4():
     result = "fooziman"
-    #...
+    last_index = len(result) -1
+    result = result[:last_index] + result[last_index].upper()
     return result
